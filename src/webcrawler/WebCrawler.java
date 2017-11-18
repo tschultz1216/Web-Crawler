29d42c43bd6465006954ff5cf4b7f45c754a5c02
Alan Miller <alanmiller@Alans-MacBook-Pro.local>
2017-11-17 19:35:02 -0500

    testing testing 123

--- src/webcrawler/WebCrawler.java
+++ src/webcrawler/WebCrawler.java
@@ -21,23 +21,21 @@ public class WebCrawler {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
-        
         // add Scanner to get first inpput link
         // parse input doc for each "href" 
         // parse input for each image file
-        
         // new doc  for each href link using Jsoup.connect(string).get()
-        
-            System.out.println("Change");
+        System.out.println("Change");
 
         Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
-Elements newsHeadlines = doc.select("#mp-itn b a");
-for (Element headline : newsHeadlines) {
+        System.out.println("hey");
+        Elements newsHeadlines = doc.select("#mp-itn b a");
+        for (Element headline : newsHeadlines) {
 //  log("%s\n\t%s", 
 //    headline.attr("title"), headline.absUrl("href"));
-}
+        }
     }
-    
+
 }
