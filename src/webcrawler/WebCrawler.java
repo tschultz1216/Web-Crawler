95b77153e0bb16f19810d82a3f1130d716aa0d0f
tschultz1216 <tschultz@unca.edu>
2017-11-27 19:33:40 -0500

    Update WebCrawler.java

--- src/webcrawler/WebCrawler.java
+++ src/webcrawler/WebCrawler.java
@@ -9,11 +9,8 @@ package webcrawler;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-<<<<<<< HEAD
 import java.util.Iterator;
-=======
 import java.util.Scanner;
->>>>>>> 82f9caa46910d3d1ea813a3f7c05242ea899e3d6
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -32,18 +29,9 @@ public class WebCrawler {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-<<<<<<< HEAD
         
-        ArrayList<WebImage> images = new ArrayList<WebImage>();
-        // TODO code application logic here
-        // add Scanner to get first inpput link
-        // parse input doc for each "href" 
-        // parse input for each image file
-        // new doc  for each href link using Jsoup.connect(string).get()
-//            System.out.println("Change");
-//
         Element doc = Jsoup.connect("https://en.wikipedia.org/wiki/Sea_mink#/media/File:The_Canadian_field-naturalist_(1988)_(20332897078).jpg").get();
-=======
+
 
         ArrayList<WebImage> images = new ArrayList<WebImage>();
 
@@ -64,8 +52,6 @@ public class WebCrawler {
             System.out.println("Directories: " + repo.getDirName() + " created");
         }
 
-        Element doc = Jsoup.connect(address).get();
->>>>>>> 82f9caa46910d3d1ea813a3f7c05242ea899e3d6
         Elements e = doc.getAllElements();
         Elements foundImages = doc.getElementsByTag("img");
         for (Element element : foundImages) {
@@ -73,24 +59,11 @@ public class WebCrawler {
 //            System.out.println(element.toString());
             images.add(wi);
         }
-<<<<<<< HEAD
         for (WebImage image : images) {
             image.saveToFile();
-        } //Elements newsHeadlines = doc.select("#mp-itn b a");
-//for (Element headline : newsHeadlines) {
-//  log("%s\n\t%s", 
-//    headline.attr("title"), headline.absUrl("href"));
+        } 
     }
 
-    Scanner scanner = new Scanner(System.in);
-    //WebPage webPage = new WebPage(scanner.next());
-
-=======
-
-        for (WebImage image : images) {
-            image.saveToFile();
-        }
-    }
->>>>>>> 82f9caa46910d3d1ea813a3f7c05242ea899e3d6
 }
+
 
